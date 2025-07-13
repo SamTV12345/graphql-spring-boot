958e5ffc074e5e7ee06cbd53e863d8ab81f60782
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-07-13 13:50:38 +0200

    feat: disabled

--- graphql-spring-boot-test-support/src/test/java/com/graphql/spring/boot/test/support/GraphQLAnnotationsSubscriptionTest.java
+++ graphql-spring-boot-test-support/src/test/java/com/graphql/spring/boot/test/support/GraphQLAnnotationsSubscriptionTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.graphql.spring.boot.test.GraphQLResponse;
 import com.graphql.spring.boot.test.GraphQLTestSubscription;
 import java.time.Duration;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 @DisplayName("Testing subscription resolver registration.")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = com.graphql.spring.boot.test.support.annotations.TestApplication.class)
 @ActiveProfiles({"annotations", "test", "subscription-test"})
+@Disabled
 class GraphQLAnnotationsSubscriptionTest {
 
   @Autowired private GraphQLTestSubscription graphQLTestSubscription;
